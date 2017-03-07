39709ce8a5bdc8c82e4e61d4d23b5403e806997c
gbuffing <gbuffing@gmail.com>
2017-03-07 17:39:13 -0600

    Trying to figure out why Jacques mcmlgo.c and mcmlio.c are complaining about undefined reference to sqrt, acos, sin, etc....math.h has been included.  ??

---
+++ ctmp/src/ctmp.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : ctmp.c
+ Author      : Gavin Buffington
+ Version     :
+ Copyright   : yes
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(void) {
+	puts("mcml"); /* prints mcml */
+	double a = sqrt(4.);
+	printf("%f", a);
+	return EXIT_SUCCESS;
+}
---
+++ mcml/src/mcml.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : mcml.c
+ Author      : Gavin Buffington
+ Version     :
+ Copyright   : yes
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main_tmp(void) {
+	puts("mcml"); /* prints mcml */
+	return EXIT_SUCCESS;
+}
